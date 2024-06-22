ac037495ba29e3eb84a4ab2ea28dfa6ded570fe4
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-06-22 10:01:03 +0300

    try-with-resources

--- jdbc/src/main/java/io/hexlet/App.java
+++ jdbc/src/main/java/io/hexlet/App.java
@@ -10,10 +10,10 @@ public class App {
         // База создается прямо во время выполнения этой строчки
         // Здесь hexlet_test — это имя базы данных
         try (Connection conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
-            String sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+            String sql1 = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
             // Чтобы выполнить запрос, создадим объект statement
             try (Statement statement = conn.createStatement()) {
-                statement.execute(sql);
+                statement.execute(sql1);
             }
 
             String sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
