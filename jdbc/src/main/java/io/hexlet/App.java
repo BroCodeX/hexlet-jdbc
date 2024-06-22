2061f93d6c9f4ca0bf15795a7e4c41d59e616a20
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-06-22 09:58:18 +0300

    init

--- jdbc/src/main/java/io/hexlet/App.java
+++ jdbc/src/main/java/io/hexlet/App.java
@@ -9,33 +9,27 @@ public class App {
         // Создаем соединение с базой в памяти
         // База создается прямо во время выполнения этой строчки
         // Здесь hexlet_test — это имя базы данных
-        Connection conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
+        try (Connection conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
+            String sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+            // Чтобы выполнить запрос, создадим объект statement
+            try (Statement statement = conn.createStatement()) {
+                statement.execute(sql);
+            }
 
-        String sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
-        // Чтобы выполнить запрос, создадим объект statement
-        Statement statement = conn.createStatement();
-        statement.execute(sql);
-        statement.close(); // В конце закрываем
+            String sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
+            try (Statement statement2 = conn.createStatement()) {
+                statement2.executeUpdate(sql2);
+            }
 
-        String sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-        Statement statement2 = conn.createStatement();
-        statement2.executeUpdate(sql2);
-        statement2.close();
-
-        String sql3 = "SELECT * FROM users";
-        Statement statement3 = conn.createStatement();
-        // Здесь вы видите указатель на набор данных в памяти СУБД
-        ResultSet resultSet = statement3.executeQuery(sql3);
-        // Набор данных — это итератор
-        // Мы перемещаемся по нему с помощью next() и каждый раз получаем новые значения
-        while (resultSet.next()) {
-            System.out.println(resultSet.getLong("id"));
-            System.out.println(resultSet.getString("username"));
-            System.out.println(resultSet.getString("phone"));
+            String sql3 = "SELECT * FROM users";
+            try (Statement statement3 = conn.createStatement()) {
+                ResultSet resultSet = statement3.executeQuery(sql3);
+                while (resultSet.next()) {
+                    System.out.printf("%s %s %s", resultSet.getLong("id"),
+                            resultSet.getString("username"),
+                            resultSet.getString("phone"));
+                }
+            }
         }
-        statement3.close();
-
-        // Закрываем соединение
-        conn.close();
     }
 }
