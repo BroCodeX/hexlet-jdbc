6a8529bc7f5bdf258b4908d0668d078a8b3aafab
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-06-24 14:25:25 +0300

    DAO pattern

--- jdbc/src/main/java/io/hexlet/App.java
+++ jdbc/src/main/java/io/hexlet/App.java
@@ -1,7 +1,14 @@
 package io.hexlet;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
 import java.sql.*;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class App {
     // Нужно указывать базовое исключение,
@@ -10,56 +17,108 @@ public class App {
         // Создаем соединение с базой в памяти
         // База создается прямо во время выполнения этой строчки
         // Здесь hexlet_test — это имя базы данных
-        try (Connection conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
+        Connection conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
+        try (Statement statement = conn.createStatement()) {
             String sql1 = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
-            // Чтобы выполнить запрос, создадим объект statement
-            try (Statement statement = conn.createStatement()) {
-                statement.execute(sql1);
-            }
+            statement.execute(sql1);
+        }
+        UserDAO dao = new UserDAO(conn);
 
-            String sql2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
-            try (PreparedStatement preparedStatement = conn.prepareStatement(sql2, Statement.RETURN_GENERATED_KEYS)) {
-                preparedStatement.setString(1, "Sarah");
-                preparedStatement.setString(2, "123456");
-                preparedStatement.executeUpdate();
+        var user = new User("Maria", "888888888");
+        var user3 = new User("Jack", "12345678");
+        user.getId(); // null
+        dao.save(user);
+        dao.save(user3);
+        System.out.println(user.getId());
+        System.out.println(user3.getId());// Здесь уже выводится какой-то id
 
-                preparedStatement.setString(1, "Max");
-                preparedStatement.setString(2, "234567");
-                preparedStatement.executeUpdate();
+        dao.printed();
+        System.out.println("=====");
 
-                preparedStatement.setString(1, "Del");
-                preparedStatement.setString(2, "99937656");
+// Возвращается Optional<User>
+        var user2 = dao.find(1L).get();
+        System.out.println(user2.getId() == user.getId()); // true
+
+        dao.del(user3);
+        dao.printed();
+        System.out.println("=====");
+
+        var user4 = dao.find(3L).orElse(new User("null","null"));
+        System.out.println(user4);
+    }
+}
+
+@AllArgsConstructor
+class UserDAO {
+    private Connection connection;
+
+    public void save(User user) throws SQLException {
+        if (user.getId() == null) {
+            String sql = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (PreparedStatement preparedStatement = this.connection
+                    .prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+                preparedStatement.setString(1, user.getUsername());
+                preparedStatement.setString(2, user.getPhone());
                 preparedStatement.executeUpdate();
 
                 var generatedKeys = preparedStatement.getGeneratedKeys();
                 if (generatedKeys.next()) {
-                    System.out.println(generatedKeys.getLong(1));
+                    user.setId(generatedKeys.getLong(1));
                 } else {
-                    throw new SQLException("DB have not returned an id after saving the entity");
-                }
-            }
-
-            String sql3 = "DELETE FROM users WHERE username = 'Del'";
-            try (PreparedStatement preparedStatement2 = conn.prepareStatement(sql3)) {
-                try (Statement statement3 = conn.createStatement()) {
-                    ResultSet resultSet = statement3.executeQuery("SELECT * FROM users");
-                    while (resultSet.next()) {
-                        System.out.printf("%s %s\n", resultSet.getLong("id"),
-                                resultSet.getString("username"));
-                    }
-                }
-                preparedStatement2.executeUpdate();
-            }
-
-            String sql4 = "SELECT * FROM users";
-            try (Statement statement3 = conn.createStatement()) {
-                ResultSet resultSet = statement3.executeQuery(sql4);
-                while (resultSet.next()) {
-                    System.out.printf("%s %s %s\n", resultSet.getLong("id"),
-                            resultSet.getString("username"),
-                            resultSet.getString("phone"));
+                    throw new SQLException("DB have not returned an id after saving an entity");
                 }
             }
         }
     }
+
+    public void del(User user) throws SQLException {
+        if (user.getId() != null) {
+            String sql = "DELETE FROM users WHERE id = ?";
+            try (PreparedStatement preparedStatement = this.connection
+                    .prepareStatement(sql)){
+                preparedStatement.setLong(1, user.getId());
+                preparedStatement.executeUpdate();
+                user.setId(null);
+        }
+    } else {
+            throw new SQLException("DB have not returned an id after saving an entity");
+        }
+    }
+
+    public Optional<User> find (Long id) throws SQLException {
+        String sql = "SELECT * FROM users WHERE id = ?";
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            var resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                String username = resultSet.getString("username");
+                String phone = resultSet.getString("phone");
+                User user = new User(username, phone);
+                user.setId(id);
+                return Optional.of(user);
+            }
+            return Optional.empty();
+        }
+    }
+
+    public void printed () throws SQLException {
+        String sql = "SELECT * FROM users";
+        try (Statement statement = this.connection.createStatement()) {
+            var resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                String username = resultSet.getString("username");
+                String phone = resultSet.getString("phone");
+                System.out.printf("%s %s\n", username, phone);
+            }
+        }
+    }
+}
+
+@Data
+@RequiredArgsConstructor
+@ToString
+class User {
+    private Long id;
+    private final String username;
+    private final String phone;
 }
