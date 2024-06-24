bd63a47ed331260e08b1e70118c293d0caee7311
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-06-24 15:02:49 +0300

    DAO pattern

--- jdbc/src/main/java/io/hexlet/App.java
+++ jdbc/src/main/java/io/hexlet/App.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
