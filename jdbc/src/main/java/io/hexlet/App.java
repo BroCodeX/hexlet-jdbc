978d4e39177615b54821b2099251372ed75edab3
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-06-24 10:15:33 +0300

    statements

--- jdbc/src/main/java/io/hexlet/App.java
+++ jdbc/src/main/java/io/hexlet/App.java
@@ -1,6 +1,7 @@
 package io.hexlet;
 
 import java.sql.*;
+import java.util.List;
 
 public class App {
     // Нужно указывать базовое исключение,
@@ -16,16 +17,45 @@ public class App {
                 statement.execute(sql1);
             }
 
-            String sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-            try (Statement statement2 = conn.createStatement()) {
-                statement2.executeUpdate(sql2);
+            String sql2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (PreparedStatement preparedStatement = conn.prepareStatement(sql2, Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, "Sarah");
+                preparedStatement.setString(2, "123456");
+                preparedStatement.executeUpdate();
+
+                preparedStatement.setString(1, "Max");
+                preparedStatement.setString(2, "234567");
+                preparedStatement.executeUpdate();
+
+                preparedStatement.setString(1, "Del");
+                preparedStatement.setString(2, "99937656");
+                preparedStatement.executeUpdate();
+
+                var generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    System.out.println(generatedKeys.getLong(1));
+                } else {
+                    throw new SQLException("DB have not returned an id after saving the entity");
+                }
             }
 
-            String sql3 = "SELECT * FROM users";
+            String sql3 = "DELETE FROM users WHERE username = 'Del'";
+            try (PreparedStatement preparedStatement2 = conn.prepareStatement(sql3)) {
+                try (Statement statement3 = conn.createStatement()) {
+                    ResultSet resultSet = statement3.executeQuery("SELECT * FROM users");
+                    while (resultSet.next()) {
+                        System.out.printf("%s %s\n", resultSet.getLong("id"),
+                                resultSet.getString("username"));
+                    }
+                }
+                preparedStatement2.executeUpdate();
+            }
+
+            String sql4 = "SELECT * FROM users";
             try (Statement statement3 = conn.createStatement()) {
-                ResultSet resultSet = statement3.executeQuery(sql3);
+                ResultSet resultSet = statement3.executeQuery(sql4);
                 while (resultSet.next()) {
-                    System.out.printf("%s %s %s", resultSet.getLong("id"),
+                    System.out.printf("%s %s %s\n", resultSet.getLong("id"),
                             resultSet.getString("username"),
                             resultSet.getString("phone"));
                 }
